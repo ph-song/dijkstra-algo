63629814b8e2b1e12885f6510f9e99e8513cec9f
ph-song <songpinhen@gmail.com>
2022-09-13 13:13:34 +1000

    solved q1

--- assignment2.py
+++ assignment2.py
@@ -9,18 +9,8 @@ from typing import List, Tuple
 from math import inf
 import heapq
 
-class LocationVertex:
-    def __init__(self)-> None:
-        """
-        location constructor
-        :input:
-            arg1: id, location id
-        :time complexity: O(1)
-        :aux space complexity:
-        """
-        self.adjacent = []
-        self.wait_time = -1
 
+#task 1
 class RoadGraph:
     def __init__(self, roads: List[Tuple], cafes:List[Tuple])->None:
         """
@@ -43,25 +33,19 @@ class RoadGraph:
         >>> cafes = [(5, 10), (6, 1), (7, 5), (0, 3), (8, 4)]
         >>> g = RoadGraph(roads, cafes)
         """
-        #find the number of locations, O(E)
-        self.num_loc = 0
-        for i in range(len(roads)):
-            loc_id =  roads[i][0] or roads[i][1] #larger number of location id
-            if loc_id+1 > self.num_loc:
-                self.num_loc = loc_id+1 #update number of vertices
+        self.num_loc = len(roads) #number of edge
 
-        #create vertices, O(V)
-        self.graph = [LocationVertex() for _ in range(self.num_loc)]
-
-        #connect edges, O(E)
-        for loc in roads:
+        #create directed weightwd graph
+        self.graph = [[] for _ in range(self.num_loc)] #create vertices, O(V)
+        for loc in roads: #connect edges, O(E)
             u, v, w = loc
-            self.graph[u].adjacent.append((v,w))
+            self.graph[u].append((v,w))
 
-        #record cafe, worst case scenario: number of cafes = V, O(V)
-        for cafe in cafes:
-            cafe_id, wait_time = cafe
-            self.graph[cafe_id].wait_time = wait_time
+        #reversed directed weighted graph
+        self.graph_rev = [[] for _ in range(self.num_loc)] #create vertices, O(V)
+        for loc in roads: #connect edges, O(E)
+            u, v, w = loc
+            self.graph_rev[v].append((u,w))
         
         self.cafes = cafes
         
@@ -91,38 +75,52 @@ class RoadGraph:
         [3, 4, 8, 7, 3, 4]
         """
         
-        start_to_cafe, cafe_to_end = [], []
-        for cafe in self.cafes:
-            start_to_cafe.append(self.shortest_path(start, cafe[0]))
-            cafe_to_end.append(self.shortest_path(cafe[0], end))
+        start_to_cafe_pred, start_to_cafe_dist= self.shortest_path(start, False)#shortest path to cafe
+        cafe_to_end_pred, cafe_to_end_dist = self.shortest_path(end, True) #shortest from cafe to destination
+    
         
+        #find shortest distance
         min_dist = inf
-        for i in range(len(self.cafes)):
-            if start_to_cafe[i][1] + cafe_to_end[i][1] + self.cafes[i][1] < min_dist:
-                min_dist = start_to_cafe[i][1] + cafe_to_end[i][1] + self.cafes[i][1]
+        for cafe in self.cafes:
+            cafe_id, wait_time = cafe
+            total_dist = start_to_cafe_dist[cafe_id] + cafe_to_end_dist[cafe_id] + wait_time
+            if total_dist < min_dist:
+                min_dist = total_dist
+                cafe_chosen = cafe_id
         
-        for i in range(len(self.cafes)):
-            if start_to_cafe[i][1] + cafe_to_end[i][1] + self.cafes[i][1] == min_dist:
-                shortest_path = start_to_cafe[i][0][:-1] #***
-                shortest_path += cafe_to_end[i][0]
-                break
-        
+        path_start_to_cafe = self.find_path(start_to_cafe_pred, cafe_chosen)
+        path_cafe_to_end = self.find_path(cafe_to_end_pred, cafe_chosen)
+
+        shortest_path = path_start_to_cafe[::-1][0:-1] + path_cafe_to_end
+
         return shortest_path
 
-    def shortest_path(self, start, end):
+    def find_path(self, pred: List, end):
+        path = [end]
+        parent = pred[end]
+        while parent != None:
+            path.append(parent)
+            parent = pred[parent]
+            
+        return path
+        
+
+    def shortest_path(self, start, rev: bool):
         #predecessor
         pred = [None for _ in range(self.num_loc)]
         dist = [inf for _ in range(self.num_loc)]
 
         #distance
         dist[start] = 0 #start = 0 
+        
+        #grpah
+        graph = self.graph_rev if rev else self.graph #start to cafe or end to cafe
 
         #priority queue
         p_queue = [(inf,i) for i in range(self.num_loc)]
         p_queue = [(0,start)] #start = 0 
         heapq.heapify(p_queue)
         p_queue_mark = [False for _ in range(self.num_loc)] #mark if a vertex has been searched
-        
 
         while bool(p_queue):
             u = heapq.heappop(p_queue)[1] #pop
@@ -131,7 +129,7 @@ class RoadGraph:
             p_queue_mark[u] = True #mark popped vertex
 
             #relax
-            for adj in self.graph[u].adjacent: #for neighbour of u
+            for adj in graph[u]: #for neighbour of u
                 v, w = adj
                 #relax
                 if dist[u] + w < dist[v]: #if new distance < current distance
@@ -139,25 +137,24 @@ class RoadGraph:
                     pred[v] = u #update predecessor
                     heapq.heappush(p_queue,(dist[v], int(v))) #add v into queue
 
-        path = [end]
-        u = end
-        while pred[u] != None:
-            path.append(pred[u])
-            u = pred[u]
-        return path[::-1], dist[end] #***complexity 
+        return pred, dist
+
+#task 2
+
+
+
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod(verbose=True)
-    """
+    
     roads = [(0, 1, 4), (1, 2, 2), (2, 3, 3), (3, 4, 1), (1, 5, 2),
             (5, 6, 5), (6, 3, 2), (6, 4, 3), (1, 7, 4), (7, 8, 2),
             (8, 7, 2), (7, 3, 2), (8, 0, 11), (4, 3, 1), (4, 8, 10)]
     cafes = [(5, 10), (6, 1), (7, 5), (0, 3), (8, 4)]
-    mygraph = RoadGraph(roads, cafes)
-    print(mygraph.routing(1, 7), '\n') #[1, 7]
-    print(mygraph.routing(7, 8), '\n') #[7, 8]
-    print(mygraph.routing(1, 3), '\n') #[1, 5, 6, 3]
-    print(mygraph.routing(1, 4), '\n') #[1, 5, 6, 4]
-    print(mygraph.routing(3, 4), '\n') #[3, 4, 8, 7, 3, 4]
-    """
+    #mygraph = RoadGraph(roads, cafes)
+    #print(mygraph.routing(1, 7), '\n') #[1, 7]
+    #print(mygraph.routing(7, 8), '\n') #[7, 8]
+    #print(mygraph.routing(1, 3), '\n') #[1, 5, 6, 3]
+    #print(mygraph.routing(1, 4), '\n') #[1, 5, 6, 4]
+    #print(mygraph.routing(3, 4), '\n') #[3, 4, 8, 7, 3, 4]
